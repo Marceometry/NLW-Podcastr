08d4ad4ce9182d4d23283691ae9572ee33719ae9
Marcelino Teixeira <marceometry@gmail.com>
2021-05-11 12:13:29 -0300

    add nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,25 @@
-import Header from "../components/Header"
-import Player from "../components/Player"
 import { PlayerContextProvider } from "../contexts/PlayerContext"
+import Router from 'next/router'
+import Nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
+import '../css/global.css'
 
-import "../css/global.css"
+Nprogress.configure({
+  showSpinner: false,
+  trickleSpeed: 300
+})
+
+Router.events.on('routeChangeStart', () => {
+  Nprogress.start()
+})
+
+Router.events.on('routeChangeComplete', () => {
+  Nprogress.done()
+})
+
+Router.events.on('routeChangeError', () => {
+  Nprogress.done()
+})
 
 function MyApp({ Component, pageProps }) {
   return (
